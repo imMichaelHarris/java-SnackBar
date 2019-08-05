0600b51e0c03484c0050566e8afb6e9aa28333ce
Michael Harris <mhguitar1997@gmail.com>
2019-08-05 13:06:21 -0700

    Create bob customer

--- src/snacks/Main.java
+++ src/snacks/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args){
         Customer jane = new Customer("Jane", 45.25);
+        Customer bob = new Customer("Bob", 33.14);
 
         System.out.println(jane.getName());
     }
