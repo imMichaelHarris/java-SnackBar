069f612296530d17d7bf67abb8335899b862b36b
Michael Harris <mhguitar1997@gmail.com>
2019-08-05 12:24:21 -0700

    Getter and Setter add for name field

--- src/snacks/Snack.java
+++ src/snacks/Snack.java
@@ -27,4 +27,10 @@ public class Snack {
     public setId(int id){
         this.id = id;
     }
+    public getName(){
+        return name;
+    }
+    public setName(String name){
+        this.name = name;
+    }
 }
