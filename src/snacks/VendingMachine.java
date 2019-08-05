e7eb0dba0fab2a7b5124343327ee30b345eaecec
Michael Harris <mhguitar1997@gmail.com>
2019-08-05 12:40:38 -0700

    Create constructor for Vending Machine

--- src/snacks/VendingMachine.java
+++ src/snacks/VendingMachine.java
@@ -1,5 +1,11 @@
 package snacks;
 
 public class VendingMachine{
-    
+    private int id;
+    private String name;
+
+    public VendingMachine(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
 }
