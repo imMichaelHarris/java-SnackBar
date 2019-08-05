72ea3e64f0a111994cf324d50f09cf56da22a650
Michael Harris <mhguitar1997@gmail.com>
2019-08-05 12:21:17 -0700

    Create constructor for Snack

--- src/snacks/Snack.java
+++ src/snacks/Snack.java
@@ -7,4 +7,16 @@ public class Snack {
     private int quanity;
     private double cost;
     private int vendingId;
+
+    //this is a constructor which will be called when we initiate a new object from this claass i.e. "new Snack()"
+
+    public Snack(String name, int quanity, double cost, int vendingId){
+        maxid++;
+
+        id = maxid;
+        this.name = name;
+        this.quanity = quanity;
+        this.cost = cost;
+        this.vendingId = vendingId;
+    }
 }
