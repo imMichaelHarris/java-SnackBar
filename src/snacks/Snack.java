1e5e5d94ce511caa3435e05d46cd235336bec821
Michael Harris <mhguitar1997@gmail.com>
2019-08-05 12:18:02 -0700

    Create private fields for snack class

--- src/snacks/Snack.java
+++ src/snacks/Snack.java
@@ -1,5 +1,10 @@
 package snacks;
 
 public class Snack {
-    
+
+    private static int maxid = 0;
+    private String name;
+    private int quanity;
+    private double cost;
+    private int vendingId;
 }
